8051a3df93ece965b53d70a50d574e4cacf16666
Ifud <Ifud@Saifuddin>
2015-04-28 13:50:45 +0700

    kamera

---
+++ src/hariiput/saifuddin.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hariiput;
+
+/**
+ *
+ * @author Ifud
+ */
+public class saifuddin {
+    public static void main(String[] args) {
+        
+    
+    int a,b,c,Hasil;
+    
+    a = 5;
+    b = 10;
+    c = 2;
+    Hasil=a*b*c;
+    
+        System.out.println("Hasil Perkalian :");
+        System.out.println(Hasil);
+    
+}
+}
