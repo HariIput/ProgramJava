fd40057b88c872c5b82329894aadfad60b7fbfe7
SBC4 <SBC4@SBC08>
2015-04-28 13:43:04 +0700

    belajarpenjumlahan

---
+++ src/hariiput/HariIput.java
@@ -0,0 +1,14 @@
+package hariiput;
+
+public class HariIput {
+
+    public static void main(String[] args) {
+       int a,b,c,hasil;
+       a = 50;
+       b = 20;
+       c = 30;
+       hasil = a+b+c ;
+        System.out.println("hasil = ");
+        System.out.println(hasil);
+    }
+}
